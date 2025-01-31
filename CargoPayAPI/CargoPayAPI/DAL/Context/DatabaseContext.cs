ed1096536386ed24402edd6d76a8669bbc8d114b
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2025-01-31 18:30:03 -0500

    Database connection created

---
+++ CargoPayAPI/CargoPayAPI/DAL/Context/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using CargoPayAPI.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CargoPayAPI.DAL.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        public DbSet<Card> Cards { get; set; }
+    }
+}
---
+++ CargoPayAPI/CargoPayAPI/Migrations/20250131232801_Init.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CargoPayAPI.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Number = table.Column<string>(type: "nvarchar(16)", maxLength: 16, nullable: false),
+                    Expires = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cvv = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: false),
+                    Brand = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cards");
+        }
+    }
+}
