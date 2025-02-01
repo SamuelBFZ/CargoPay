b1303b9fe80c74d5397d9cf74e7456473da21ed3
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2025-02-01 15:10:37 -0500

    Endpoints DeleteCardAsync and GetBalanceAsync created

--- CargoPayAPI/CargoPayAPI/Controllers/CardController.cs
+++ CargoPayAPI/CargoPayAPI/Controllers/CardController.cs
@@ -39,12 +39,39 @@ namespace CargoPayAPI.Controllers
             return Ok(card);
         }
 
+        [HttpGet, ActionName("GetBalance")]
+        [Route("GetBalance")]
+        public async Task<ActionResult> GetBalanceAsync(Guid id)
+        {
+            try
+            {
+                var balance = await _cardService.GetBalanceAsync(id);
+
+                return Ok($"Your current balance is: {balance}");
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
         [HttpPost, ActionName("Create")]
         [Route("Create")]
         public async Task<ActionResult> CreateCardAsync(Card card)
         {
             var createdCard = await _cardService.CreateCardAsync(card);
             return Ok(createdCard);
+        }
+
+        [HttpDelete, ActionName("Delete")]
+        [Route("Delete")]
+        public async Task<ActionResult> DeleteCardAsync(Guid id)
+        {
+            var deletedCard = await _cardService.DeleteCardAsync(id);
+
+            if (deletedCard == null) return NotFound("Card ID not found");
+
+            return Ok($"{deletedCard.Number} deleted");
         }
     }
 }
