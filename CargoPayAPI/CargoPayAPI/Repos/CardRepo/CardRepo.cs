f6f16e724eebf6673ecb6614aff20f5ada2f34c3
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2025-02-01 11:51:32 -0500

    CardRepo Implementated

---
+++ CargoPayAPI/CargoPayAPI/Repos/CardRepo/CardRepo.cs
@@ -0,0 +1,81 @@
+﻿using CargoPayAPI.DAL.Context;
+using CargoPayAPI.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CargoPayAPI.Repos.CardRepo
+{
+    public class CardRepo : ICardRepo
+    {
+        private readonly DatabaseContext _context;
+
+        public CardRepo(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Card> GetCardByIdAsync(Guid id)
+        {
+            return await _context.Cards.FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<IEnumerable<Card>> GetCardsAsync()
+        {
+            return await _context.Cards.ToListAsync();
+        }
+
+        public async Task<Card> CreateCardAsync(Card card)
+        {
+            try
+            {
+                card.Id = Guid.NewGuid();
+
+                _context.Cards.Add(card);
+                await _context.SaveChangesAsync();
+
+                return card;
+
+            }catch(DbUpdateException ex)
+            {
+                throw new Exception(ex.InnerException?.Message ?? ex.Message);
+            }
+        }
+
+        public async Task<Card> UpdateCardAsync(Card card)
+        {
+            try
+            {
+                _context.Cards.Update(card);
+                await _context.SaveChangesAsync();
+
+                return card;
+
+            }catch(DbUpdateException ex)
+            {
+                throw new Exception(ex.InnerException?.Message ?? ex.Message);
+            }
+        }
+
+        public async Task<Card> DeleteCardAsync(Guid id)
+        {
+            var card = await _context.Cards.FirstOrDefaultAsync(b => b.Id == id);
+            if (card == null) return null;
+
+            _context.Cards.Remove(card);
+            await _context.SaveChangesAsync();
+
+            return card;
+        }
+
+        public async Task<decimal> GetBalanceAsync(Guid id)
+        {
+            var card = await _context.Cards.FirstOrDefaultAsync(b => b.Id == id);
+            if (card == null) throw new KeyNotFoundException($"ID {id} Not found");
+            return card.Balance;
+        }
+
+        public async Task<bool> HasSufficientBalanceAsync(Guid id, decimal amount)
+        {
+            return await _context.Cards.AnyAsync(c => c.Id == id && c.Balance >= amount);
+        }
+    }
+}
