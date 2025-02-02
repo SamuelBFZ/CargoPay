bc13f77eee9abfbdaa35bfa34a8821335c6bda69
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2025-02-02 17:59:09 -0500

    Entity User (relation less) created

---
+++ CargoPayAPI/CargoPayAPI/DAL/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace CargoPayAPI.DAL.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set;}
+        public string PasswordHash { get; set;}
+        public string Email { get; set;}
+        public string RefreshToken { get; set;}
+        public DateTime RefreshTokenExpiryTime { get; set;} 
+    }
+}
